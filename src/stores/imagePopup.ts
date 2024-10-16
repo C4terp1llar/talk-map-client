13e4514fd951db91882b5773e4d15f1c1abb36da
C4terp1llar <m0skvitin@mail.ru>
2024-10-16 05:51:05 +0300

    рефикс попап имг

--- src/stores/imagePopup.ts
+++ src/stores/imagePopup.ts
@@ -4,6 +4,7 @@ import {ref} from "vue";
 const lockScroll = () => {
     const scrollY = window.scrollY;
     document.body.style.position = 'fixed';
+    document.body.style.width = '100%';
     document.body.style.top = `-${scrollY}px`;
     document.body.dataset.scrollY = scrollY.toString();
 }
@@ -12,6 +13,7 @@ const unlockScroll = () => {
     const scrollY = document.body.dataset.scrollY || '0';
     document.body.style.position = '';
     document.body.style.top = '';
+    document.body.style.width = '';
     window.scrollTo(0, parseInt(scrollY));
     document.body.dataset.scrollY = '';
 }
